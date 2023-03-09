64cf4ee96c1eb51e9d60f4abfd6daceab480786f
giomikadze <110746808+giomikadze@users.noreply.github.com>
2023-03-09 21:06:00 +0100

    lesson-5

---
+++ script.js
@@ -0,0 +1,104 @@
+
+
+//         DOGS
+
+// function dog(number,string) {
+//   let obj = {};
+//   for(let i = 0;i <=number;i++){
+//    obj[string + i] = `${string}${i}`
+//   }
+//   return obj;
+// }
+// console.log(dog(3,"dog"));
+
+
+
+// четные и нечетные числа 
+
+// const arr = [5, 9, 6, 8, 7, 4, 1];
+// let sum = 0;
+// function even(arr) {
+// for(let i = 0;i <= arr.length; i++){
+//   if(arr[i] % 2 === 0){
+//      sum += arr[i];
+//     }
+//   }
+// }
+
+// even(arr);
+// console.log(sum)
+
+
+
+
+
+// let sum = 0;
+// function ofEven(arr) {
+// for(let i = 0;i < arr.length; i++){
+//   if(arr[i] % 2 !==0){
+//     sum += arr[i];
+//     }
+//   }
+
+// }
+// ofEven(arr)
+// console.log(sum)
+
+
+
+
+
+
+// function compare(sumEven,sumEven) {
+//   if(sumEven > sumEven){
+//     console.log(sumEven)
+//   }else {
+//     console.log(sumOdd)
+//   }
+// }
+// console.log(sumOdd)
+
+
+
+
+// const faringit = 32;
+// let celsia1 = 0;
+// function calc(celsia){
+//   celsia1 = (celsia * 9 / 5) + faringit;
+
+// console.log(celsia1)
+// }
+// calc(1)
+
+
+
+// const arr = [2, 3, 5, 7, 8, 4, 9];
+
+// let max = arr[0];
+// function value(arr) {
+// for(let i = 0; i < arr.length; i++){
+//   if(arr[i] > max){
+//     max = arr[i]
+//   }
+// }
+// console.log(max)
+// }
+
+// value(arr)
+
+
+
+
+// const arr = [ 3, 5, 7, 8, 4, 9, 2];
+
+// let min = arr[0];
+// function value(arr) {
+// for(let i = 0; i < arr.length; i++){
+//   if(arr[i] < min){
+//     min = arr[i]
+//   }
+// }
+// console.log(min)
+// }
+
+// value(arr)
